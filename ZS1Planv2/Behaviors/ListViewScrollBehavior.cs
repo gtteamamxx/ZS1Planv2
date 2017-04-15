c76855ee23c510ca9d6fbcef2236e5149c13d85e
gtteamamxx <grs4_98@o2.pl>
2017-04-15 10:53:45 +0200

    Added scrolling at listview when user pressed back button

---
+++ ZS1Planv2/Behaviors/ListViewScrollBehavior.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xaml.Interactivity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace ZS1Planv2.Behaviors
+{
+    public class ListViewScrollBehavior : Behavior<ListView>
+    {
+        protected override void OnAttached()
+        {
+            ListView listView = base.AssociatedObject;
+            listView.SelectionChanged += ListView_SelectionChanged;
+        }
+
+        protected override void OnDetaching()
+        {
+            ListView listView = base.AssociatedObject;
+            listView.SelectionChanged -= ListView_SelectionChanged;
+        }
+
+        private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selectedItem = ((ListView)sender).SelectedItem;
+            if (selectedItem == null)
+                return;
+            ((ListView)sender).ScrollIntoView(selectedItem);
+        }
+    }
+}
